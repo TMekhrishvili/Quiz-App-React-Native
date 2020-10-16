32907a2c84b96703f7334de9203d8aa113d845ef
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-10-17 00:40:24 +0400

    no message

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Quiz App</Text>
+    </SafeAreaView>
   );
 }
 
