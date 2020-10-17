e504010f57f11bfe4ad6074fab5a0c43f50cd299
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2020-10-17 16:53:34 +0400

    Horizontal slideshow

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { ScrollView, StyleSheet, Image, Text, View, Dimensions, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+const image = [
+  'https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2335126/pexels-photo-2335126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2335126/pexels-photo-2335126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2440024/pexels-photo-2440024.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  'https://images.pexels.com/photos/2335126/pexels-photo-2335126.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+]
+const { width } = Dimensions.get("window");
+const height = width * 0.6;
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Quiz App</Text>
-    </SafeAreaView>
+    <View style={{ marginTop: 25, width, height }}>
+      <ScrollView pagingEnabled horizontal style={{ width, height }}>
+        {
+          image.map(
+            (value, index) => (
+              <Image
+                key={index}
+                source={{ uri: value }}
+                style={{ width, height }}
+              />
+            ))
+        }
+      </ScrollView>
+      <Text style={{ color: '#000' }}>swipe...</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
